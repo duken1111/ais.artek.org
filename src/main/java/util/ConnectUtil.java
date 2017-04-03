6780e765f67c36a403a6ba0c5379945152f65dba
dukman <dukman.box@gmail.com>
2017-04-03 08:30:33 +0300

    last modifired

--- src/main/java/util/ConnectUtil.java
+++ src/main/java/util/ConnectUtil.java
@@ -7,6 +7,7 @@ import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -28,7 +29,10 @@ import java.io.UnsupportedEncodingException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.net.CookieManager.*;
 
@@ -37,10 +41,12 @@ import static java.net.CookieManager.*;
  * Created by DLepeshko on 14.03.2017.
  */
 public class ConnectUtil {
-
+    private static final String URL = "http://artek.org";
     private final String USER_AGENT = "Mozilla/5.0";
-    private HttpClient client = HttpClientBuilder.create().build();
+    RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(30 * 1000).build();
+    private HttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
     private String cookies;
+    private static String urlPagingTemplate = "http://artek.org/admin/news/news/?p=";
 
 
     public static void main(String[] args) throws Exception {
@@ -58,15 +64,64 @@ public class ConnectUtil {
 
         conn.sendPost(url, params);
 
-        String main = conn.GetPageContent(mainPage);
+        Integer linksCount = conn.getAllPageLinks(mainPage + "news/news/");
+        //Thread.sleep(1000);
 
-        System.out.println(main);
 
+
+
+        //List<String> links = conn.getNewsLinks(linksCount);
+        List<String> link = new ArrayList<>();
+        link.add("http://artek.org/admin/news/news/583/");
+        //System.out.println("Всего:" + links.size());
+
+        conn.makeChange(link);
+
+    }
+
+    private void makeChange(List<String> links) throws Exception {
+        for(String url : links) {
+            String page = GetPageContent(url);
+            sendPost(url,getFormParamsNews(page));
+        }
+    }
+
+    private int getAllPageLinks(String url) throws Exception {
+
+        String page = GetPageContent(url);
+        Document doc = Jsoup.parse(page);
+        Element el = doc.select(".pagination ul li").last();
+
+        Integer totalPages = Integer.parseInt(el.text());
+        System.out.println(totalPages);
+        System.out.println("-------------------------------------------");
+
+
+        return totalPages;
+    }
+
+    private List<String> getNewsLinks(Integer totalPages) throws Exception {
+        List<String> links = new ArrayList<>();
+        for (int i = 0; i < totalPages; i++) {
+            String pageUrl = urlPagingTemplate + i;
+            String page = GetPageContent(pageUrl);
+
+            Document doc = Jsoup.parse(page);
+            Elements elements = doc.select("#result_list tbody tr");
+
+            for(Element el : elements) {
+                Element a = el.select(".field-title a").first();
+                links.add(URL + a.attributes().get("href"));
+            }
+
+        }
+
+        return links;
     }
 
     private void sendPost(String url, List<NameValuePair> postParams)
             throws Exception {
-
+        System.out.println("try to send post");
         HttpPost post = new HttpPost(url);
 
         // add header
@@ -82,6 +137,7 @@ public class ConnectUtil {
 
         post.setEntity(new UrlEncodedFormEntity(postParams));
 
+
         HttpResponse response = client.execute(post);
 
         int responseCode = response.getStatusLine().getStatusCode();
@@ -95,10 +151,9 @@ public class ConnectUtil {
 
         StringBuilder result = new StringBuilder();
 
-        rd.lines().forEach(s -> result.append(s).append("\n"));
+        //rd.lines().forEach(s -> result.append(s).append("\n"));
 
-        System.out.println(result.toString());
-
+        post.releaseConnection();
     }
 
 
@@ -112,7 +167,9 @@ public class ConnectUtil {
                 "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
         request.setHeader("Accept-Language", "en-US,en;q=0.5");
 
+
         HttpResponse response = client.execute(request);
+
         int responseCode = response.getStatusLine().getStatusCode();
 
         System.out.println("\nSending 'GET' request to URL : " + url);
@@ -126,14 +183,13 @@ public class ConnectUtil {
         rd.lines().forEach(s -> result.append(s).append("\n"));
 
         // set cookies
-        for(Header header : response.getAllHeaders()) {
-            System.out.println(header.getName() + " - " + header.getValue());
-        }
-        System.out.println();
+
         this.setCookies(response.getFirstHeader("Set-Cookie") == null ? "" :
                 response.getFirstHeader("Set-Cookie").toString());
         //System.out.println(getCookies());
+        request.releaseConnection();
         return result.toString();
+
     }
 
     public List<NameValuePair> getFormParams(String html, String username, String password)throws UnsupportedEncodingException {
@@ -158,6 +214,36 @@ public class ConnectUtil {
                 value = password;
 
             paramList.add(new BasicNameValuePair(key, value));
+
+        }
+
+        return paramList;
+    }
+
+    public List<NameValuePair> getFormParamsNews(String html)throws UnsupportedEncodingException {
+
+        Document doc = Jsoup.parse(html);
+
+        // Google form id
+        Element loginform = doc.select("#news_form").first();
+
+        List<NameValuePair> paramList = new ArrayList<NameValuePair>();
+
+        //Все input
+        Elements inputElements = loginform.getElementsByTag("input");
+        for (Element inputElement : inputElements) {
+
+                String key = inputElement.attr("name");
+                String value = inputElement.attr("value");
+                if(key.equals("title_en")) {
+                    value = "LOLOLOLOLOOOLOOOLOOOL";
+                    System.out.println(value);
+                }
+
+
+                paramList.add(new BasicNameValuePair(key, value));
+
+
 
         }
 
